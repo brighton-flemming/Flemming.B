fbd99cd4c56a20389fb2b6b14118147358ae624e
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-24 02:49:41 +0300

    Mission Passed

--- python-code-challenge-superheroes/code-challenge/db/seed.py
+++ python-code-challenge-superheroes/code-challenge/db/seed.py
@@ -1,35 +1,71 @@
-puts "🦸‍♀️ Seeding powers..."
-Power.create([
-  { name: "super strength", description: "gives the wielder super-human strengths" },
-  { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  { name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
-  { name: "elasticity", description: "can stretch the human body to extreme lengths" }
-])
+# puts "🦸‍♀️ Seeding powers..."
+# Power.create([
+#   { name: "super strength", description: "gives the wielder super-human strengths" },
+#   { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
+#   { name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
+#   { name: "elasticity", description: "can stretch the human body to extreme lengths" }
+# ])
 
-puts "🦸‍♀️ Seeding heroes..."
-Hero.create([
-  { name: "Kamala Khan", super_name: "Ms. Marvel" },
-  { name: "Doreen Green", super_name: "Squirrel Girl" },
-  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
-  { name: "Janet Van Dyne", super_name: "The Wasp" },
-  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
-  { name: "Carol Danvers", super_name: "Captain Marvel" },
-  { name: "Jean Grey", super_name: "Dark Phoenix" },
-  { name: "Ororo Munroe", super_name: "Storm" },
-  { name: "Kitty Pryde", super_name: "Shadowcat" },
-  { name: "Elektra Natchios", super_name: "Elektra" }
-])
+# puts "🦸‍♀️ Seeding heroes..."
+# Hero.create([
+#   { name: "Kamala Khan", super_name: "Ms. Marvel" },
+#   { name: "Doreen Green", super_name: "Squirrel Girl" },
+#   { name: "Gwen Stacy", super_name: "Spider-Gwen" },
+#   { name: "Janet Van Dyne", super_name: "The Wasp" },
+#   { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
+#   { name: "Carol Danvers", super_name: "Captain Marvel" },
+#   { name: "Jean Grey", super_name: "Dark Phoenix" },
+#   { name: "Ororo Munroe", super_name: "Storm" },
+#   { name: "Kitty Pryde", super_name: "Shadowcat" },
+#   { name: "Elektra Natchios", super_name: "Elektra" }
+# ])
 
-puts "🦸‍♀️ Adding powers to heroes..."
+# puts "🦸‍♀️ Adding powers to heroes..."
 
-strengths = ["Strong", "Weak", "Average"]
-Hero.all.each do |hero|
-  rand(1..3).times do
-    # get a random power
-    power = Power.find(Power.pluck(:id).sample)
+# strengths = ["Strong", "Weak", "Average"]
+# Hero.all.each do |hero|
+#   rand(1..3).times do
+#     # get a random power
+#     power = Power.find(Power.pluck(:id).sample)
 
-    HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
-  end
-end
+#     HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
+#   end
+# end
 
-puts "🦸‍♀️ Done seeding!"
+# puts "🦸‍♀️ Done seeding!"
+
+
+from random import randint
+
+from app.app import db
+from app.models import Hero, HeroPower, Power
+
+
+def SeedingPowers():
+    message = print("🦸‍♀️ Seeding powers...")
+    return f"<h2>{ message }</h2>"
+
+powers_data = [
+    {"name": "super strength", "description": "gives the wielder super-human strengths"},
+    {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
+    {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
+    {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+]
+
+for power_info in powers_data:
+    power = Power(**power_info)
+    db.session.add(power)
+
+    db.session.commit()
+
+    def SeedingPowersDone():
+        message = print("🦸‍♀️ Seeding powers... Done!")
+        return f"<h2>{ message }</h2>"
+
+def SeedingHeroes():
+    message = print("🦸‍♀️ Seeding heroes...")
+    return f"<h2>{ message }</h2>"
+
+
+
+
