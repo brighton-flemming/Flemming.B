27f451deba355d1fc91db956989dec6264ffcc93
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-24 02:09:21 +0300

    Mission Passed

--- python-code-challenge-superheroes/code-challenge/app/models.py
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -9,16 +9,25 @@ class Hero(db.Model):
     name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String)
 
-    powers = db.relationship('Power',secondary='hero_power' backref='heroes')
+    powers = db.relationship('Power',secondary='hero_power' back_populates='heroes')
 
+class HeroPower(db.Model):
+    __tablename__ = 'hero_power'
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), primary_key=True)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), primary_key=True)
+
+    hero = db.relationship('Hero', back_populates='heroes')
+    power = db.relationship('Power', back_populates='powers') 
 
 class Power(db.Model):
     __tablename__ = 'powers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
 
-    heroes = db.relationship('Hero', backref='powers')
+    heroes = db.relationship('Hero', secondary='hero_power', back_populates='powers')
 
 
     
