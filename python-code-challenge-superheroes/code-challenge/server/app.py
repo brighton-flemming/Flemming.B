b885f415167ad9a84fedb7e6e297beaf7aa965c2
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-24 14:18:47 +0300

    MIssion Passed

--- python-code-challenge-superheroes/code-challenge/server/app.py
+++ python-code-challenge-superheroes/code-challenge/server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Hero
@@ -9,13 +9,33 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+heroes= [
+    {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+    {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
+    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
+    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
+    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
+    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
+    {"name": "Ororo Munroe", "super_name": "Storm"},
+    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
+    {"name": "Elektra Natchios", "super_name": "Elektra"}
+]
+
+powers = [
+    {"name": "super strength", "description": "gives the wielder super-human strengths"},
+    {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
+    {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
+    {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+]
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    return jsonify(heroes)
 
 
 if __name__ == '__main__':
